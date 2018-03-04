83b570d36c602b27d08e158052e09c44644b0d60
Shelton Cai <shelton408@gmail.com>
2018-03-04 11:05:06 -0500

    Edited main to allow easier change of image used in program

--- main.m
+++ main.m
@@ -1,14 +1,13 @@
+%change this line of code to the image you want to test this code on
 dataDir = fullfile('img','test1.jpg');
+
 im1 = rgb2gray(imread(dataDir));
 figure(1); clf;
 
-dataDir = fullfile('img','test2.jpg');
-im2 = rgb2gray(imread(dataDir));
 sigma = 4;
 k=4;
 
-%change im1 here to change what image is used
-im1 = double(im2)/255;
+im1 = double(im1)/255;
 subplot(3,3,1); imshow(im1); axis image off; title('grayscale im');
 
 subplot(3,3,2); imshow(clustersTest(im1,2)); axis image off; title('k=2 cluster');
